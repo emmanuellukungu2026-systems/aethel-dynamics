f9d57dd2259b7f8780dd97e6b05c2fea7c6437c7
emmanuellukungu2026-systems <emmanuellukungu6@gmail.com>
2026-08-04 03:45:23 +0300

    je viens d'actualiser

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def page_home():
     return render_template('index.html')
+@app.route('/signin')
+def signin_page():
+    return render_template('signin.html')
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
